4444a542226b273d4fa662bc18a634b3c93f0280
Arsema-Zeweldi <arsugeb@gmail.com>
2022-03-10 08:35:53 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,6 @@
  *
  * Return: Always 0 (Sucess)
  */
-
 int main(void)
 {
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
@@ -13,6 +12,5 @@ int main(void)
 	printf("Size of an long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
-
 	return (0);
 }
