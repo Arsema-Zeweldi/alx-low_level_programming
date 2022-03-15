d5db327ae467d5a5fc3914e0cc320aaf8bcf5dc0
Arsema-Zeweldi <arsugeb@gmail.com>
2022-03-15 12:21:06 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - <zero>
+ * @n: The int that checks the code
+ * Return: -1, 0, 1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+	_putchar('\n');
+}
