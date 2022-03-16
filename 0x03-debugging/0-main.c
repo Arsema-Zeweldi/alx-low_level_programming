8ee64ae511f76a044301a0046ce97b89a6a892f0
Arsema-Zeweldi <arsugeb@gmail.com>
2022-03-16 04:39:59 -0700

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * positive_or_negative - tests positivity or negativity
+ *
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+
+	i = 98;
+	positive_or_negative(i);
+
+	return;
+}
