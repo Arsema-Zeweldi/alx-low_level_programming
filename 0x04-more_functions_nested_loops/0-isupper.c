010b88d377f0666c28ca7e0e9f9bcea814a5ee9b
Arsema-Zeweldi <arsugeb@gmail.com>
2022-03-17 01:28:59 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - Checks for uppercase characters
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
