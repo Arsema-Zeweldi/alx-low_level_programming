0094a014a25643847b8844a5776d8c8d9969b2ba
Arsema-Zeweldi <arsugeb@gmail.com>
2022-03-17 09:52:06 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,37 +1,32 @@
 #include <stdio.h>
 /**
- * main - fizz-buzz test
+ * main - checks the code for ALX students.
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int c;
-	char f[] = "Fizz";
-	char b[] = "Buzz";
-	char fb[] = "FizzBuzz";
 
 	for (c = 1; c <= 100; c++)
-		if (c == 100)
+	{
+		if (c % 3 == 0)
 		{
-			printf("%s ", b);
-		}
-		else if (c % 15 == 0)
-		{
-			printf("%s ", fb);
+			printf("Fizz ");
 		}
 		else if (c % 5 == 0)
 		{
-			printf("%s ", b);
+			printf("Buzz ");
 		}
-		else if (c % 3 == 0)
+		else if (c % 15 == 0)
 		{
-			printf("%s ", f);
+			printf("FizzBuzz ");
 		}
 		else
 		{
 			printf("%d ", c);
 		}
+	}
 	printf("\n");
 	return (0);
 }
