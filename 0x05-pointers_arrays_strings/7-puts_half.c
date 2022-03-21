1018f17c8d26fc0aaa866e9bdc867415166135e9
Arsema-Zeweldi <arsugeb@gmail.com>
2022-03-21 09:41:39 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+		;
+
+	c++;
+	for (c /= 2; str[c] != '\0'; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+}
