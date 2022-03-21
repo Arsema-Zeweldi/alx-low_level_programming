89fc61808205880089ac292b01542e58acec2b72
Arsema-Zeweldi <arsugeb@gmail.com>
2022-03-21 07:01:15 -0700

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * _puts - prints a string
- * @str: string
+ * _puts - prints a string to stdout
+ * @str: string to be evaluated
  * Return: void
  */
 void _puts(char *str)
