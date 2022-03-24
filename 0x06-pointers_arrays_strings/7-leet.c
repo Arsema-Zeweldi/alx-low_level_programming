b688ab037ff4aa5db6cb4459177d555ffc1d23bf
Arsema-Zeweldi <arsugeb@gmail.com>
2022-03-24 10:21:37 -0700

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *leet - encodes a string into 1337
+ * @s: string
+ * Return: char
+ */
+char *leet(char *s)
+{
+	int c;
+	int d;
+	char s1[] = "ol_ea__t";
+	
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		for (d = 0; s1[d] != '\0'; d++)
+		{
+			if (s[c] == s1[d] || s[c] == (s1[d] - 32))
+			{
+				s[c] = d + 48;
+			}
+		}
+	}
+	return (s);
+}
