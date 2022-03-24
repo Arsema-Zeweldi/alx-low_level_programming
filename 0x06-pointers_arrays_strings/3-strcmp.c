8dc6490ad684cea02d1cae9f3ccf3cd756a3ab04
Arsema-Zeweldi <arsugeb@gmail.com>
2022-03-24 09:20:48 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: Hello
+ * @s2: World!
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int c;
+
+	for (c = 0; s1[c] != '\0' || s2[c] != '\0'; c++)
+	{
+		if (s1[c] != s2[c])
+		{
+			return (s1[c] - s2[c]);
+		}
+	}
+	return (0);
+}
