b75e0fcd3251a1e5d95ece2b86fcf91b870e0f36
Arsema-Zeweldi <arsugeb@gmail.com>
2022-04-01 07:29:59 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: size of argv
+ * @argv: contains command line arguments
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
