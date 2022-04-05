8002a2a8180c16e515c20f7e36311ed1877715b6
Arsema-Zeweldi <arsugeb@gmail.com>
2022-04-05 09:16:49 -0700

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		ptr[i] = malloc(width * sizeof(**ptr));
 		if (ptr[i] == NULL)
 		{
-			while (--i > 0)
+			while (--i >= 0)
 				free(ptr[i]);
 			free(ptr);
 			return (NULL);
