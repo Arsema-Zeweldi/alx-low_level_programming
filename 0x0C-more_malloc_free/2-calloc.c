a8bd567c529f789f2186144d0663e0af9b58b8ad
Arsema-Zeweldi <arsugeb@gmail.com>
2022-04-07 09:46:42 -0700

    2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_calloc - allocates memory
+ * @nmemb: int 1
+ * @size: int 2
+ * Return: void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int c;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	c = nmemb * size;
+	ptr = malloc(c * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	else
+		return(ptr);
+}
