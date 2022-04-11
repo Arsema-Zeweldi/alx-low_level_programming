ead3d4145c9670e361cf52e601ed0a587b1480bb
Arsema-Zeweldi <arsugeb@gmail.com>
2022-04-11 07:37:26 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,24 +10,24 @@
 void print_dog(struct dog *d)
 {
 
-	if (d == NULL)
-		exit(1);
-
-	if (d->name != NULL)
+	if (d != NULL)
 	{
-		printf("Name : %s\n", d->name);
-	}
-	else
-	{
-		printf("nil");
-	}
-	printf("Age : %f\n", d->age);
-	if (d->owner != NULL)
-	{
-		printf("Owner: %s\n", d->owner);
-	}
-	else
-	{
-		printf("nil");
+		if (d->name != NULL)
+		{
+			printf("Name : %s\n", d->name);
+		}
+		else
+		{
+			printf("nil");
+		}
+		printf("Age : %f\n", d->age);
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("nil");
+		}
 	}
 }
