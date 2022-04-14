b5e5a387003448161a724e77b9252edbba879b36
Arsema-Zeweldi <arsugeb@gmail.com>
2022-04-14 06:31:55 -0700

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,16 +9,18 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list ap;
 	unsigned int i;
 	int c = 0;
 
 	if (n == 0)
 		return (0);
-	va_list ap;
-
+	
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
+	{
 		c += va_arg(ap, int);
+	}
 	va_end(ap);
 
 	return (c);
