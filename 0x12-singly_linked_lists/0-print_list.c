6cea177e2781703df642a0a14984cec570ec0fe8
Arsema-Zeweldi <arsugeb@gmail.com>
2022-04-21 11:55:48 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,12 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nbr_nodes = 0;
-	const list_t *current_node = h;
+	size_t i;
 
-	while (current_node != NULL)
+	for (i = 0; h; i++)
 	{
-		printf("[%d] %s\n", current_node->len,
-		current_node->str != NULL ? current_node->str : "(nil)");
-		current_node = current_node->next;
-		nbr_nodes++;
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
 	}
-	return (nbr_nodes);
+	return (i);
 }
