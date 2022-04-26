8b17d156ca730cf5e85421bbe954a0d1fb187cbe
Arsema-Zeweldi <arsugeb@gmail.com>
2022-04-26 04:07:51 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,38 +1,49 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
+
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
- * @head: head of the list
- * @index: place to insert node
- * @n: value of the inserted node
- * Return: pointer to head of list
+ * @head: double pointer to the head of the listint_t linked list
+ * @idx: the index of the list where the new node should be added
+ * @n: element/property n of the node to be added
+ * Return: address of the new element (SUCCESS), or
+ * NULL if it failed (FAILURE), or
+ * NULL if is not possible to add the new node at index @idx
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
-{
-	listint_t *current, *new;
 
-	current = *head;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	if (index == 0)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *current_node = *head, *new_node_ptr;
+
+	if (idx == 0)
 	{
-		new->next = current;
-		*head = new;
-		return (*head);
-	}
-	while (index > 1)
-	{
-		current = current->next;
-		index--;
-		if (!current)
-		{
-			free(new);
+		new_node_ptr = malloc(sizeof(listint_t));
+
+		if (!new_node_ptr)
 			return (NULL);
-		}
+		new_node_ptr->n = n;
+		new_node_ptr->next = *head;
+		*head = new_node_ptr;
+		return (new_node_ptr);
 	}
-	new->next = current->next;
-	current->next = new;
-	return (new);
+
+	while (current_node && ((i + 1) != idx))
+	{
+		current_node = current_node->next;
+		i++;
+	}
+
+	if ((i + 1) == idx)
+	{
+		new_node_ptr = malloc(sizeof(listint_t));
+
+		if (!new_node_ptr)
+			return (NULL);
+		new_node_ptr->n = n;
+		new_node_ptr->next = current_node->next;
+		current_node->next = new_node_ptr;
+		return (new_node_ptr);
+	}
+	return (NULL);
 }
