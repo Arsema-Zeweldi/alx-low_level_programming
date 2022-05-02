64ed6e73fa7a6009c5b91b61789813c8dab8b442
Arsema-Zeweldi <arsugeb@gmail.com>
2022-05-02 05:16:55 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,7 @@
  * @letters: number of letters
  * Return: number of letters
  */
-ssize_t read_textfile (const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t read_c, write_c;
@@ -14,7 +14,7 @@ ssize_t read_textfile (const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
