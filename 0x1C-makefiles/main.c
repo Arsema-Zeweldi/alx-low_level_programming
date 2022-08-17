718676f29725aff8d53badf69e464164e8176734
Arsema-Zeweldi <arsugeb@gmail.com>
2022-08-17 02:13:15 -0700

    0 & 1

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,11 @@
+#include "makefile.h"
+
+/**
+ * main - main
+ * Return: int
+ */
+int main(void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
---
+++ 0x1C-makefiles/makefile.h
@@ -0,0 +1,9 @@
+#ifndef _MAKEFILE_H_
+#define _MAKEFILE_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_school(void);
+
+#endif
